d7b93441a23c071a4ce5b2e87445b3656abd2ee2
acast23 <antoniocastillob@hotmail.com>
2021-09-03 13:08:03 -0500

    primer version users (controller, model,route)
    Además de modificaciones a objetos de user, se proponen algunos nombres dentro de index como lo son:
    -anuncios = ads
    -solicitudes= bids

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+// CRUD Structure
+const router = require('express').Router();
+const {
+  createUser,
+  getUsers,
+  modifyUser,
+  deleteUser
+} = require('../controllers/users')
+
+router.get('/', getUsers)
+router.post('/', createUser)
+router.put('/:id', modifyUser)
+router.delete('/:id', deleteUser)
+
+module.exports = router;
