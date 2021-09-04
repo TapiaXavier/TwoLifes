f347edacf863b32f5b256b15b6b8f82bbb34a8e1
Ana Castillo <66333350+canastillo@users.noreply.github.com>
2021-09-03 19:33:59 -0500

    LÃogica dentro del entry point

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-let router = require('express').Router()
-
-router.get('/', (req, res) => {
-    res.send('Welcome to TwoLifes')
-})
-
-router.use('/ads', require('./ads'))
-
-module.exports = router
