bff4608c13e02a8f1bc957b273a6943ba85d64ef
acast23 <antoniocastillob@hotmail.com>
2021-09-24 15:32:38 -0500

    Users V2 + extra
    Users files + passport addition, (package-lock, package json and routes/index.js) modifications

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
+// Express
 const express = require('express')
 const app = express()
 
+//Body Parser
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+//DB Config
+const mongoose = require('mongoose');
+
+/*mongoose.connect(
+    //process.env.MONGODB_URI
+    //{ useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+	);*/
+
+mongoose.set("debug", true)
+
+require('./models/Ad')
+require('./models/User')
+
+require('./models/PurchaseRequest')
+require('./models/Videogame')
+//require('./config/Videogame')
+
+//require('./config/passport')
+
+//Routes
 app.use('/v1', require('./routes'));
 
-const PORT = 4001
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`)
-})
+
+//Server iniciation
+/*app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}`)*/
+//})
