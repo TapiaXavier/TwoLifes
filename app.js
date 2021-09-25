4480ff228dd1422b80b644d17accbf7087a2036e
Devmmanuel <plasmmadev@gmail.com>
2021-09-24 20:44:39 -0500

    pequeños cambios

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require('express')
 const app = express()
 
 const bodyParser = require('body-parser')
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 app.use(bodyParser.json())
+
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://admin:C28mc7iSC0BvHLjD@cluster0.tzwzo.mongodb.net/twolifes?retryWrites=true&w=majority", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+
+mongoose.set("debug", true)
+
+require('./models/Videogame');
 
 app.use('/v1', require('./routes'));
 
