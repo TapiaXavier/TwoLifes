be04f8c4ce19b6f0e160a776ffd3c13b7ac39aef
Tapia <zs17012957@estudiantes.uv.mx>
2021-09-26 20:09:33 -0500

    Se crea modelo de purchaseRequest

--- models/PurchaseRequest.js
+++ models/PurchaseRequest.js
@@ -1,3 +1,27 @@
+const mongoose = require('mongoose');
+
+const PurchaseRequestSchema = new mongoose.Schema({
+    idAdvertiser: {type: mongoose.Schema.Types.ObjectId, ref: 'user_col', required: true},
+    idUser: {type: mongoose.Schema.Types.ObjectId, ref: 'user_col', required: true},
+    relaseDate: {type: Date, required: true},
+    status: {type: String, enum: ['Aceptada', 'En espera', 'Negada']},
+    deliveryDate: {type: Date}
+}, {timestamps: true, collection: 'purchase_col'} )
+
+PurchaseRequestSchema.methods.publicData = function(){
+    return {
+        advertiser: this.idAdvertiser,
+        user: this.idUser,
+        relaseDate: this.relaseDate,
+        status: this.status,
+        deliveryDate: this.deliveryDate
+    }
+    
+}
+
+mongoose.model('purchase', PurchaseRequestSchema);
+
+/*
 class PurchaseRequest {
     constructor(idAd, idUser, relaseDate, status, deliveryDate) {
         this.idAd = idAd;
@@ -9,3 +33,5 @@ class PurchaseRequest {
 }
 
 module.exports = PurchaseRequest;
+*/
+
